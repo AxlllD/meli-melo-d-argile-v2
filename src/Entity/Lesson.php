c3b200124342c79cdd06ad2e6eabf034e55cf936
Axel Delabroye <delabroyeaxel@gmail.com>
2023-12-13 11:22:40 +0100

    Initial commit.

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?float $Price = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Items = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Adress = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(float $Price): static
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getItems(): ?string
+    {
+        return $this->Items;
+    }
+
+    public function setItems(?string $Items): static
+    {
+        $this->Items = $Items;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->Adress;
+    }
+
+    public function setAdress(string $Adress): static
+    {
+        $this->Adress = $Adress;
+
+        return $this;
+    }
+}
